8e24ee6e611703b9df9fcad551b049da8b0f565a
Zeebiii <mathias.msl@outlook.fr>
2019-07-07 13:24:44 +0200

    Ajout des boutons du menu.

--- Training/Form1.Designer.cs
+++ Training/Form1.Designer.cs
@@ -28,13 +28,239 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MenuItemFichier = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemFichierOuvrir = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemFichierFermer = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemQuitter = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemFichierOuvrirMemory = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemFichierFermerFenetreActive = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemFichierFermerToutesLesFenetre = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemFenetre = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemAPropos = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.ToolStripBtnOuvrirMemory = new System.Windows.Forms.ToolStripButton();
+            this.ToolStripBtnJsp = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ToolStripBtnQuitterFenetreActive = new System.Windows.Forms.ToolStripButton();
+            this.ToolStripBtnFemerToutesLesFenetre = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.ToolStripBtnCascade = new System.Windows.Forms.ToolStripButton();
+            this.ToolStripBtnMirrorVertical = new System.Windows.Forms.ToolStripButton();
+            this.ToolStripBtnMirrorHorizontal = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemFichier,
+            this.MenuItemFenetre,
+            this.MenuItemAPropos});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MenuItemFichier
+            // 
+            this.MenuItemFichier.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemFichierOuvrir,
+            this.MenuItemFichierFermer,
+            this.MenuItemQuitter});
+            this.MenuItemFichier.Name = "MenuItemFichier";
+            this.MenuItemFichier.Size = new System.Drawing.Size(54, 20);
+            this.MenuItemFichier.Text = "Fichier";
+            // 
+            // MenuItemFichierOuvrir
+            // 
+            this.MenuItemFichierOuvrir.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemFichierOuvrirMemory});
+            this.MenuItemFichierOuvrir.Name = "MenuItemFichierOuvrir";
+            this.MenuItemFichierOuvrir.Size = new System.Drawing.Size(180, 22);
+            this.MenuItemFichierOuvrir.Text = "Ouvrir";
+            // 
+            // MenuItemFichierFermer
+            // 
+            this.MenuItemFichierFermer.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemFichierFermerFenetreActive,
+            this.MenuItemFichierFermerToutesLesFenetre});
+            this.MenuItemFichierFermer.Name = "MenuItemFichierFermer";
+            this.MenuItemFichierFermer.Size = new System.Drawing.Size(180, 22);
+            this.MenuItemFichierFermer.Text = "Fermer";
+            // 
+            // MenuItemQuitter
+            // 
+            this.MenuItemQuitter.Name = "MenuItemQuitter";
+            this.MenuItemQuitter.Size = new System.Drawing.Size(180, 22);
+            this.MenuItemQuitter.Text = "Quitter";
+            // 
+            // MenuItemFichierOuvrirMemory
+            // 
+            this.MenuItemFichierOuvrirMemory.Name = "MenuItemFichierOuvrirMemory";
+            this.MenuItemFichierOuvrirMemory.Size = new System.Drawing.Size(180, 22);
+            this.MenuItemFichierOuvrirMemory.Text = "Memory";
+            // 
+            // MenuItemFichierFermerFenetreActive
+            // 
+            this.MenuItemFichierFermerFenetreActive.Name = "MenuItemFichierFermerFenetreActive";
+            this.MenuItemFichierFermerFenetreActive.Size = new System.Drawing.Size(180, 22);
+            this.MenuItemFichierFermerFenetreActive.Text = "Fenetre active";
+            // 
+            // MenuItemFichierFermerToutesLesFenetre
+            // 
+            this.MenuItemFichierFermerToutesLesFenetre.Name = "MenuItemFichierFermerToutesLesFenetre";
+            this.MenuItemFichierFermerToutesLesFenetre.Size = new System.Drawing.Size(180, 22);
+            this.MenuItemFichierFermerToutesLesFenetre.Text = "Toutes les fenetres";
+            // 
+            // MenuItemFenetre
+            // 
+            this.MenuItemFenetre.Name = "MenuItemFenetre";
+            this.MenuItemFenetre.Size = new System.Drawing.Size(58, 20);
+            this.MenuItemFenetre.Text = "Fenetre";
+            // 
+            // MenuItemAPropos
+            // 
+            this.MenuItemAPropos.Name = "MenuItemAPropos";
+            this.MenuItemAPropos.Size = new System.Drawing.Size(67, 20);
+            this.MenuItemAPropos.Text = "A propos";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripBtnOuvrirMemory,
+            this.ToolStripBtnJsp,
+            this.toolStripSeparator1,
+            this.ToolStripBtnQuitterFenetreActive,
+            this.ToolStripBtnFemerToutesLesFenetre,
+            this.toolStripSeparator2,
+            this.ToolStripBtnCascade,
+            this.ToolStripBtnMirrorVertical,
+            this.ToolStripBtnMirrorHorizontal});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // ToolStripBtnOuvrirMemory
+            // 
+            this.ToolStripBtnOuvrirMemory.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ToolStripBtnOuvrirMemory.Image = ((System.Drawing.Image)(resources.GetObject("ToolStripBtnOuvrirMemory.Image")));
+            this.ToolStripBtnOuvrirMemory.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ToolStripBtnOuvrirMemory.Name = "ToolStripBtnOuvrirMemory";
+            this.ToolStripBtnOuvrirMemory.Size = new System.Drawing.Size(23, 22);
+            this.ToolStripBtnOuvrirMemory.Text = "toolStripButton1";
+            // 
+            // ToolStripBtnJsp
+            // 
+            this.ToolStripBtnJsp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ToolStripBtnJsp.Image = ((System.Drawing.Image)(resources.GetObject("ToolStripBtnJsp.Image")));
+            this.ToolStripBtnJsp.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ToolStripBtnJsp.Name = "ToolStripBtnJsp";
+            this.ToolStripBtnJsp.Size = new System.Drawing.Size(23, 22);
+            this.ToolStripBtnJsp.Text = "toolStripButton2";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // ToolStripBtnQuitterFenetreActive
+            // 
+            this.ToolStripBtnQuitterFenetreActive.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ToolStripBtnQuitterFenetreActive.Image = ((System.Drawing.Image)(resources.GetObject("ToolStripBtnQuitterFenetreActive.Image")));
+            this.ToolStripBtnQuitterFenetreActive.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ToolStripBtnQuitterFenetreActive.Name = "ToolStripBtnQuitterFenetreActive";
+            this.ToolStripBtnQuitterFenetreActive.Size = new System.Drawing.Size(23, 22);
+            this.ToolStripBtnQuitterFenetreActive.Text = "toolStripButton3";
+            // 
+            // ToolStripBtnFemerToutesLesFenetre
+            // 
+            this.ToolStripBtnFemerToutesLesFenetre.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ToolStripBtnFemerToutesLesFenetre.Image = ((System.Drawing.Image)(resources.GetObject("ToolStripBtnFemerToutesLesFenetre.Image")));
+            this.ToolStripBtnFemerToutesLesFenetre.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ToolStripBtnFemerToutesLesFenetre.Name = "ToolStripBtnFemerToutesLesFenetre";
+            this.ToolStripBtnFemerToutesLesFenetre.Size = new System.Drawing.Size(23, 22);
+            this.ToolStripBtnFemerToutesLesFenetre.Text = "toolStripButton4";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // ToolStripBtnCascade
+            // 
+            this.ToolStripBtnCascade.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ToolStripBtnCascade.Image = ((System.Drawing.Image)(resources.GetObject("ToolStripBtnCascade.Image")));
+            this.ToolStripBtnCascade.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ToolStripBtnCascade.Name = "ToolStripBtnCascade";
+            this.ToolStripBtnCascade.Size = new System.Drawing.Size(23, 22);
+            this.ToolStripBtnCascade.Text = "toolStripButton5";
+            // 
+            // ToolStripBtnMirrorVertical
+            // 
+            this.ToolStripBtnMirrorVertical.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ToolStripBtnMirrorVertical.Image = ((System.Drawing.Image)(resources.GetObject("ToolStripBtnMirrorVertical.Image")));
+            this.ToolStripBtnMirrorVertical.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ToolStripBtnMirrorVertical.Name = "ToolStripBtnMirrorVertical";
+            this.ToolStripBtnMirrorVertical.Size = new System.Drawing.Size(23, 22);
+            this.ToolStripBtnMirrorVertical.Text = "toolStripButton6";
+            // 
+            // ToolStripBtnMirrorHorizontal
+            // 
+            this.ToolStripBtnMirrorHorizontal.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ToolStripBtnMirrorHorizontal.Image = ((System.Drawing.Image)(resources.GetObject("ToolStripBtnMirrorHorizontal.Image")));
+            this.ToolStripBtnMirrorHorizontal.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ToolStripBtnMirrorHorizontal.Name = "ToolStripBtnMirrorHorizontal";
+            this.ToolStripBtnMirrorHorizontal.Size = new System.Drawing.Size(23, 22);
+            this.ToolStripBtnMirrorHorizontal.Text = "toolStripButton7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFichier;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFichierOuvrir;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFichierOuvrirMemory;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFichierFermer;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFichierFermerFenetreActive;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFichierFermerToutesLesFenetre;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemQuitter;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFenetre;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemAPropos;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton ToolStripBtnOuvrirMemory;
+        private System.Windows.Forms.ToolStripButton ToolStripBtnJsp;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton ToolStripBtnQuitterFenetreActive;
+        private System.Windows.Forms.ToolStripButton ToolStripBtnFemerToutesLesFenetre;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton ToolStripBtnCascade;
+        private System.Windows.Forms.ToolStripButton ToolStripBtnMirrorVertical;
+        private System.Windows.Forms.ToolStripButton ToolStripBtnMirrorHorizontal;
     }
 }
 
